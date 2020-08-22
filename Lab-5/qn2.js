a339f47c7102f6726d9e30a00719dfef053ceb13
haresrv <haresrv2000@gmail.com>
2020-08-22 16:29:44 +0530

    Rem Labs

---
+++ Lab-5/qn2.js
@@ -0,0 +1,13 @@
+function assignGrade(score) {
+    if (score > 90) {
+        return 'A';
+    } else if (score > 80) {
+        return 'B';
+    } else if (score > 70) {
+        return 'C';
+    } else if (score > 65) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
---
+++ Lab-5/qn3.js
@@ -0,0 +1,9 @@
+var inputCelsius  = prompt("Enter Temp in 'C");;
+var outputFahrenheit = (inputCelsius * 9)/5 + 32;
+alert(inputCelsius + '\u00B0 Celsious is ' + outputFahrenheit + '\u00B0 Fahrenheit');
+console.log(inputCelsius + '\u00B0 Celsious is ' + outputFahrenheit + '\u00B0 Fahrenheit');
+
+var inputFahrenheit  = prompt("Enter Temp in 'F");
+var outputCelsius = (inputFahrenheit - 32)/9 * 5;
+alert(inputFahrenheit + '\u00B0 Fahrenheit is ' + outputCelsius + '\u00B0 Celsious.');
+console.log(inputFahrenheit + '\u00B0 Fahrenheit is ' + outputCelsius + '\u00B0 Celsious.');
